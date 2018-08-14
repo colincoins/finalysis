b2f6a81d1add482563edab239d1c4ea44de11c4c
Colin Chen <colinchen.me@gmail.com>
2018-08-13 22:34:24 -0700

    Added Logging Middleware

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -1,23 +1,45 @@
 require('dotenv').config();
 const {ApolloServer} = require('apollo-server');
 const {Prisma} = require('prisma-binding');
+const {applyMiddleware} = require('graphql-middleware');
+const {makeExecutableSchema} = require('graphql-tools');
 const resolvers = require('./resolvers');
 const typeDefs = require('./schema');
 
-const server = new ApolloServer({
-  cors: false,
-  typeDefs,
-  resolvers,
-  context: (req) => ({
-    ...req,
-    prisma: new Prisma({
-      typeDefs: 'src/generated/prisma.graphql',
-      endpoint: process.env.PRISMA_URL,
+async function main() {
+  const logInputMiddleware = async (resolve, root, args, context, info) => {
+    console.log(`1 -> logInputMiddleware - Input arguments:  ${JSON.stringify(args)}`);
+    const result = await resolve(root, args, context, info);
+    console.log(`5 -> logInputMiddleware`);
+    return result;
+  };
+
+  const logResultMiddleware = async (resolve, root, args, context, info) => {
+    console.log(`2 -> logResultMiddleware`);
+    const result = await resolve(root, args, context, info);
+    console.log(`4 -> logResultMiddleware - Result:  ${JSON.stringify(result)}`);
+    return result;
+  };
+
+  const schema = makeExecutableSchema({typeDefs, resolvers});
+  const schemaWithMiddleware = applyMiddleware(schema, logInputMiddleware, logResultMiddleware);
+  const server = new ApolloServer({
+    cors: false,
+    // typeDefs,
+    // resolvers,
+    schema: schemaWithMiddleware,
+    context: (req) => ({
+      ...req,
+      prisma: new Prisma({
+        typeDefs: 'src/generated/prisma.graphql',
+        endpoint: process.env.PRISMA_URL,
+      }),
     }),
-  }),
-});
+  });
 
-server.listen({port: process.env.PORT}).then(({url}) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
+  server.listen({port: process.env.PORT}).then(({url}) => {
+    console.log(`🚀  Server ready at ${url}`);
+  });
+};
 
+main();
